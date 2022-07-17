3f10f7d2adec62eb94fe3eb974dad5ba0da5b4f5
Nacho Martin <martin.nacho.31@gmail.com>
2022-07-17 13:40:34 +0200

    Add controller

---
+++ controllers/weatherDataController.js
@@ -0,0 +1,39 @@
+const weatherData = require("../models/weatherDataModel");
+
+const serverError = require("../utils/error500")
+
+async function getData({query}, res){
+    try {
+        const weatherEntries = await weatherData.find(query);
+        if(!weatherEntries){
+            return res.status(404)
+        }
+        return res.status(200).json(weatherEntries);
+    } catch (error) {
+        serverError(res);
+    }
+};
+
+async function deleteData({query}, res){
+    try {
+        const deletedWeatherEntries = await weatherData.deleteMany(query);
+        res.status(200).json(deletedWeatherEntries);
+    } catch (error) {
+        serverError(res);
+    }
+};
+
+async function createData({body}, res){
+    try {
+        const newWeatherEntry = await weatherData.create(body);
+        res.status(201).json(newWeatherEntry)
+    } catch (error) {
+        serverError(res);
+    }
+};
+
+module.exports ={
+    getData,
+    deleteData,
+    createData
+};
