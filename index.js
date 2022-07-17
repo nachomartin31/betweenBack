d752d3b3ada5d1e48de63b16da92a3763b9f827f
Nacho Martin <martin.nacho.31@gmail.com>
2022-07-17 13:43:39 +0200

    Add index

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+const debug = require("debug")("between");
+const morgan = require("morgan");
+
+require("./config/mongooseConfig");
+
+const server = express();
+const port = process.env.PORT || 4001;
+
+
+server.use(morgan("dev"));
+server.use(express.json());
+
+const weatherRouter = require("./routes/weatherRoute");
+server.use("/api", weatherRouter);
+
+server.listen(port, () => { debug(`Server is running on port ${port}`); });
+
