7fc8cc75cd23aa6f28bc6041fa4bba0942c6ba22
Nacho Martin <martin.nacho.31@gmail.com>
2022-07-18 15:32:09 +0200

    Add cors

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const debug = require("debug")("between");
 const morgan = require("morgan");
+const cors = require("cors")
 
 require("./config/mongooseConfig");
 
 const server = express();
 const port = process.env.PORT || 4001;
 
-
+server.use(cors())
 server.use(morgan("dev"));
 server.use(express.json());
 
